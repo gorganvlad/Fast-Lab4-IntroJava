2e79d74f98a55ae02e691d3e402b28e23f209927
Vlad <akkay96@gmail.com>
2017-03-18 13:15:21 +0200

    am mai adaugat chestii

---
+++ src/Exercitiul1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Vlad on 18.03.2017.
+ */
+public class Exercitiul1 {
+    public static void main(String[] args) {
+
+        int numar;
+        numar=SkeletonJava.readIntGUI("Introdu ma un numar:");
+
+    for(int i=0;i<=10;i++) {
+        SkeletonJava.printConsole(i+"x"+numar+"="+i *numar);
+}
+}
+
+
+
+
+
+
+
+}
